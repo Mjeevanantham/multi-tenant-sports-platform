0f2a197a4fa34e143c6ce21c69d6e9a8f9eebc3a
Jeevanantham M <mjeevanantham04@gmail.com>
2025-12-07 14:55:42 +0530

    Update TypeScript declarations to better handle Astro's class attribute

--- src/astro.d.ts
+++ src/astro.d.ts
@@ -2,11 +2,40 @@
 
 // Suppress TypeScript errors for Astro's class attribute
 // Astro uses 'class' (not 'className') which is correct HTML syntax
-declare global {
-  namespace astroHTML.JSX {
-    interface IntrinsicElements {
-      [elem: string]: any;
-    }
+// This is a known limitation: TypeScript checks Astro files as React/JSX
+
+// Extend React types to accept 'class' for Astro compatibility
+declare namespace React {
+  interface HTMLAttributes<T> extends React.AriaAttributes, React.DOMAttributes<T> {
+    class?: string;
+  }
+  
+  interface SVGProps<T> extends React.SVGAttributes<T>, React.Attributes {
+    class?: string;
+  }
+  
+  interface ButtonHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    class?: string;
+  }
+  
+  interface ImgHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    class?: string;
+  }
+  
+  interface FormHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    class?: string;
+  }
+  
+  interface InputHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    class?: string;
+  }
+  
+  interface AnchorHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    class?: string;
+  }
+  
+  interface DetailedHTMLProps<E extends HTMLAttributes<T>, T> extends HTMLAttributes<T> {
+    class?: string;
   }
 }
 
